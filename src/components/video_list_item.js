e7988ce2ecf2d1a5cfabd22791cb1f31141fa7ee
Todd Libby <toddlibby@protonmail.com>
2020-09-29 10:12:53 -0400

    Remove quotes from imgAlt in video_list_item.js

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -9,7 +9,7 @@ const VideoListItem = ({video, onVideoSelect}) => {
     <li onClick ={() => onVideoSelect(video)} className="list-group-item" tabindex="0">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" src={imgUrl} alt="{imgAlt}"/>
+          <img className="media-object" src={imgUrl} alt={imgAlt}/>
         </div>
         <div className="media-body">
           <div className="media-heading video-title">{video.snippet.title}</div>
