c87ab72fa09991fa0cb13bdf5344d07c0b951521
condro <anukumu099@gmail.com>
2021-10-02 11:14:51 +0700

    cp2940 done contributing

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'XXXXXXXXXXXX'; // REPLACE IT WITH YOUR YOUTUBE API KEY
+const API_KEY = 'AIzaSyD7H8qNnJEgXE5-ivSUzxM-Wi4AgNo0TF8'; // REPLACE IT WITH YOUR YOUTUBE API KEY
 
 
 // YTSearch({key : API_KEY, term : 'Katrina Kaif'}, function(data){
